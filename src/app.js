4ea686803e940dd5a3ce804dc5a67c3ad069f1a1
GameMaster <businesswitharmando@gmail.com>
2022-08-30 20:47:30 -0500

    Add modules, for adding items and the involvment API of microverse

--- src/app.js
+++ src/app.js
@@ -1,14 +1,68 @@
-const storeShows = [];
-const createGame = async (i) => {
-  const loopShows = `https://api.tvmaze.com/shows/${i}`;
-  const response = await fetch(loopShows, {
-    method: 'GET',
+const createLike = async (createIDShow) => {
+  const involvmentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+  const response = await fetch(involvmentApi, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   });
-  const data = await response.json();
-  if (data.status !== 404) {
-    storeShows.push(data);
-  }
-  return storeShows;
+  const data = await response.text();
+  localStorage.setItem(createIDShow, data);
+  return data;
 };
 
-export default createGame;
+const createItem = async (ID, event) => {
+  const involvmentApi = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${ID}/likes/`;
+  const responseGet = await fetch(involvmentApi, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const dataGet = await responseGet.text();
+  const responsePost = await fetch(involvmentApi, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: ID,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const dataPost = await responsePost.text();
+
+  if (dataPost === 'Created') {
+    const getParents = event.target.parentElement.firstChild.nextSibling.className;
+    let getItemsNum = 0;
+    if (dataGet !== '') {
+      getItemsNum = JSON.parse(dataGet)[0].likes;
+      getItemsNum = Number(getItemsNum) + 1;
+    } else {
+      getItemsNum = 1;
+    }
+    const sumLikes = getItemsNum;
+    const concaString = `${sumLikes} Likes`;
+    document.getElementsByClassName(getParents)[0].innerHTML = concaString;
+  }
+};
+
+const getLikes = async (ID) => {
+  const involvmentApi = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${ID}/likes/`;
+  const responseGet = await fetch(involvmentApi, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const dataGet = await responseGet.text();
+  if (dataGet !== '') {
+    // const getParents = event.target.parentElement.firstChild.nextSibling.className;
+    const getItemsNum = JSON.parse(dataGet);
+    const sumLikes = getItemsNum[0].likes;
+    console.log('sumLikes', sumLikes);
+    // const concaString = `${sumLikes} Likes`;
+    // document.getElementsByClassName(getParents)[0].innerHTML = concaString;
+  }
+};
+
+export { createLike, createItem, getLikes };
--- src/main.js
+++ src/main.js
@@ -1,25 +1,57 @@
 import './style.css';
-import createGame from './app';
+import createApp from './addlike';
+import { createLike, createItem, getLikes } from './app';
 
 let getShowsList;
 document.getElementsByClassName('shows')[0].addEventListener('click', () => {
-  for (let i = 1; i <= 250; i += 1) {
-    getShowsList = createGame(i);
-  }
-  getShowsList.then((data) => {
-    const sortObj = data.sort((a, b) => {
-      const rest = b.rating.average - a.rating.average;
-      return rest;
-    });
-    for (let i = 0; i < 6; i += 1) {
-      const imageBestShows = sortObj[i].image.medium;
-      const getParentNode = document.getElementById('content');
-      const createDivImg = document.createElement('div');
-      createDivImg.classList.add('line');
-      createDivImg.style.backgroundImage = `url(${imageBestShows})`;
-      getParentNode.appendChild(createDivImg);
-      const createDiv = document.createElement('div');
-      createDiv.classList.add('title');
+  const getLocalStorageCount = localStorage.length;
+  if (getLocalStorageCount === 0) {
+    for (let i = 1; i <= 250; i += 1) {
+      getShowsList = createApp(i);
     }
-  });
+    getShowsList.then((data) => {
+      const sortObj = data.sort((a, b) => {
+        const rest = b.rating.average - a.rating.average;
+        return rest;
+      });
+      for (let i = 0; i < 6; i += 1) {
+        const imageBestShows = sortObj[i].image.medium;
+        const nameBestShows = sortObj[i].name;
+        const ratingBestShows = sortObj[i].rating.average;
+        const craeteComment = 'Comments';
+        const craeteReserv = 'Reservations';
+        const getParentNode = document.getElementById('content');
+        const countLikes = 0;
+        const createIDShow = `Api${(i + 1)}`;
+        createLike(createIDShow).then((ID) => {
+          const createTemplate = `
+            <div class="line" style="background-image: url(${imageBestShows});"></div>
+            <div class="title">${nameBestShows}</div>
+            <div class="rating">Rating: ${ratingBestShows}</div>
+            <div class="like_cont">
+              <div id=${ID} class="count_likes${i + 1}">${countLikes} Likes</div>
+              <div class="material-symbols-outlined">
+                favorite
+              </div>
+            </div>
+            <button class="comments">${craeteComment}</button>
+            <button class="reserves">${craeteReserv}</button>
+          `;
+          const createParentEl = document.createElement('div');
+          createParentEl.classList.add('show');
+          createParentEl.innerHTML = createTemplate;
+          getParentNode.appendChild(createParentEl);
+        });
+      }
+    });
+  }
+});
+
+document.addEventListener('click', (event) => {
+  const getEventType = event.target.className;
+  const getLocalStorage = localStorage.length;
+  const getDOMId = event.target.parentElement.firstElementChild.id;
+  if (getLocalStorage > 0 && getEventType === 'material-symbols-outlined') {
+    createItem(getDOMId, event);
+  }
 });
